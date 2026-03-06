3289ddfc42e7aadd2572a2ed44b30163c6a59376
Zulfiqar Gauhar <goharzulfiqar25@gmail.com>
2026-03-06 10:52:15 +0500

    Find the Length of Array

---
+++ LenghtofArray.java
@@ -0,0 +1,6 @@
+public class LenghtofArray { //ZULFIQAR GAUHAR SAP ID 76579
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 30, 40, 50};
+        System.out.println("Length of array: " + arr.length);
+    }
+}
