a37e61eb45fa1bc55d259f0656e633b1d634623f
Zulfiqar Gauhar <goharzulfiqar25@gmail.com>
2026-03-06 10:56:03 +0500

    Find the no is Palindrom OR Not for java

---
+++ PalindromeCheck.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; //ZULFIQAR GAUHAR SAP ID: 76579
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        int originalNum = num;
+        int reversedNum = 0;
+
+        while (num != 0) { //ZULFIQAR GAUHAR SAP ID: 76579
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+
+        if (originalNum == reversedNum) {
+            System.out.println(originalNum + " is a palindrome number");
+        } else {
+            System.out.println(originalNum + " is not a palindrome number");
+        }
+    }
+}
